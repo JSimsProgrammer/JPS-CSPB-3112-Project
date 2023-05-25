587044a1b17803c193c1f61d6b59efbec5c2b232
JSimsProgrammer <JordanSimsProgrammer@gmail.com>
2023-05-25 11:23:02 -0600

    Update Week 1 to have a bouncing ball as opposed to static ball

--- week1sketch.js
+++ week1sketch.js
@@ -1,9 +1,35 @@
+class PVector {
+  constructor(x_, y_){
+    this.x = x_;
+    this.y = y_;
+  }
+
+  add(v) {
+    this.y = this.y + v.y;
+    this.x = this.x + v.x;
+  }
+}
+
+let locationVar = new PVector(100,100);
+let velocityVar = new PVector(2.5, 5);
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  ellipse(width/2, height/2, 15)
+  background(0);
+  locationVar.add(velocityVar);
+  
+  if ((locationVar.x > width-8) || (locationVar.x < 0+8)) {
+    velocityVar.x = velocityVar.x * -1;
+  }
+  if ((locationVar.y > height-8) || (locationVar.y < 0+8)) {
+    velocityVar.y = velocityVar.y * -1;
+  }
+
+  stroke(255);
+  fill(175);
+  ellipse(locationVar.x,locationVar.y,16,16);
+
 }
